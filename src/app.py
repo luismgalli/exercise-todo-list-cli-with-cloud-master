0d82721506f8b106662d8fb689178fbcb39ed746
Luis <lgallardo@austrointernet.cl>
2020-05-11 18:13:26 -0400

    Ejercicio listo

--- src/app.py
+++ src/app.py
@@ -1,5 +1,7 @@
-import json, requests
+import csv
+
 todos = []
+stop = False
 
 def get_todos():
     global todos
@@ -7,18 +9,14 @@ def get_todos():
 
 def add_one_task(title):
     global todos
-    x = {
-        'label': title,
-        'done' : False
-    }
+    x = title
     todos.append(x)
     return todos
 
 def print_list():
-    # your code here
     global todos
     for row in todos:
-        print(json.dumps(row))
+        print(row)
     pass
 
 def delete_task(number_to_delete):
@@ -27,58 +25,51 @@ def delete_task(number_to_delete):
     todos.pop(int(number_to_delete)-1)
     return todos
 
-def initialize_todos():
-    global todos
-    header={'Content-Type':'application/json'}
-    r = requests.get('https://assets.breatheco.de/apis/fake/todos/user/fulanito') 
-    if(r.status_code == 404):
-        print("No previous todos found, starting a new todolist")
-        r = requests.post(url = 'https://assets.breatheco.de/apis/fake/todos/user/fulanito', headers=header,data = json.dumps([])) 
-        if r.status_code == 200:
-            print("Tasks initialized successfully")
-    else:
-        print("A todo list was found, loading the todos...")
-        todos = r.json()
-
-    
 def save_todos():
     # your code here
     global todos
-    x=json.dumps(todos)
-    print(x)
-    header={'Content-Type':'application/json'}
-    req=requests.put("https://assets.breatheco.de/apis/fake/todos/user/fulanito", headers=header, data=x)
+    todos_file = ""
+    i = 0
 
-    print(req.json())
-    print(req)
+    for elemento in todos:
+        if i == 0:
+            todos_file = todos_file + elemento 
+            i=i+1
+        else:
+            todos_file = todos_file + ',' +  elemento 
+
+        file_to_save = open("todos_file.csv",'w+')
+        file_to_save.write(todos_file)
+        file_to_save.close()
     pass
 
-def load_todos():
-    # your code here
-    global todos
-    header={'Content-Type':'application/json'}
-    req=requests.get("https://assets.breatheco.de/apis/fake/todos/user/fulanito", headers=header, data = json.dumps(todos))
-
-    if req.status_code==200:
-        print(req.json()) 
-        todos = req.json()
-    else:
-        print(req.status_code)
-    return todos
     
+def load_todos():
+    global todos
+    file = open("todos_file.csv","r")
+    csv_1 = csv.reader(file)
+
+
+    for i in csv_1:
+        print(i)
+        for x in i:
+            if x not in todos:
+                print(x)
+                todos.append(x)  
+             
+    pass        
+    #return todos
+
 # Below this code will only run if the entry file running was app.py
 if __name__ == '__main__':
-    stop = False
-    print("Initializing todos with previous data or creating a new todo's list...")
-    initialize_todos()
     while stop == False:
         print("""
     Choose an option: 
         1. Add one task
         2. Delete a task
         3. Print the current list of tasks
-        4. Send/Save todo's to API
-        5. Retrieve todo's from API
+        4. Save todo's to todos.csv
+        5. Load todo's from todos.csv
         6. Exit
     """)
         response = input()
